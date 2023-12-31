d779b3aa71ada912a20b09dffc6d579f35f7aeca
yutopp <yutopp@gmail.com>
2023-12-31 18:11:10 +0900

    Use BackendURL. etc

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,10 +3,9 @@ import { KoyaServiceClient } from "./proto/api/v1/server.client";
 import { GrpcWebFetchTransport } from "@protobuf-ts/grpcweb-transport";
 import React from 'react'
 import { Terminal } from 'xterm'
+import { config } from "./constants";
 import './App.css'
 import 'xterm/css/xterm.css';
-
-const BACKEND_URL = "http://localhost:9000";
 
 type HeaderProps = {
   executionState: boolean;
@@ -16,7 +15,7 @@ type HeaderProps = {
 const Header = ({ executionState, handleButtonClick }: HeaderProps) => {
   return (
     <header className="py-4 bg-blue-500 text-white text-center flex justify-between items-center">
-      <h1 className="text-2xl font-bold ml-4">Proclet</h1>
+      <h1 className="text-2xl font-bold ml-4">Proclet {config.PROD ? "" : "(dev)"}</h1>
       <button className="px-4 py-2 bg-white text-blue-500 rounded mr-4" onClick={handleButtonClick}>{executionState ? "!" : "?"}Run</button>
     </header>
   );
@@ -27,6 +26,7 @@ function App() {
   const termRef = React.useRef<{ term: Terminal, open: boolean }>({ term: new Terminal({convertEol: true}), open: false });
   const abortControllerRef = React.useRef<AbortController>(new AbortController());
   const [executionState, setExecutionState] = React.useState(false);
+  const [mainSource, setMainSource] = React.useState("ulimit -a; uname -a; whoami; sleep 5; echo hello");
 
   React.useEffect(() => {
     if (termRef.current.open == false) {
@@ -35,16 +35,16 @@ function App() {
     }
   }, [])
 
-  const fetchData = async (signal: AbortSignal) => {
+  const fetchData = async (source: string, signal: AbortSignal) => {
     const client = new KoyaServiceClient(
       new GrpcWebFetchTransport({
-        baseUrl: BACKEND_URL,
+        baseUrl: config.BACKEND_URL,
       })
     );
 
     try {
       const call = client.runOneshot({
-        code: "ulimit -a; uname -a; whoami; sleep 5; echo hello",
+        code: source,
       }, {
         abort: signal,
       });
@@ -74,7 +74,8 @@ function App() {
     const newAbortController = new AbortController();
     abortControllerRef.current = newAbortController;
 
-    fetchData(newAbortController.signal).finally(() => {
+    const source = mainSource;
+    fetchData(source, newAbortController.signal).finally(() => {
       setExecutionState(false);
     });
   };
@@ -91,6 +92,8 @@ function App() {
             <textarea
               className="w-full h-full p-4 border rounded"
               placeholder="Enter your text here..."
+              defaultValue={mainSource}
+              onChange={(e) => setMainSource(e.target.value)}
             />
           </div>
 
---
+++ frontend/src/constants.ts
@@ -0,0 +1,4 @@
+export const config = {
+    PROD: import.meta.env.PROD,
+    BACKEND_URL: import.meta.env.VITE_BACKEND_URL,
+}
